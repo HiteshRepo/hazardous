77b9d6e62c874890918663faa120c4076a1aae34
hiteshrepo <pattanayak.hitesh03@gmail.com>
2024-10-26 19:29:09 +0530

    adds file utils

---
+++ pkg/helpers/fileutils.go
@@ -0,0 +1,21 @@
+package helpers
+
+import "strings"
+
+func IsAllowedExtension(filename string, allowedExts []string) bool {
+	for _, ext := range allowedExts {
+		if strings.HasSuffix(filename, ext) {
+			return true
+		}
+	}
+	return false
+}
+func IsExcludedDir(path string, excludedDirs []string) bool {
+	for _, exclude := range excludedDirs {
+		if strings.Contains(path, exclude) {
+			return true
+		}
+	}
+
+	return false
+}
